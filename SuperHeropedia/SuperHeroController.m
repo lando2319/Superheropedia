03444bcc1d0fd18443647b4997a2f77d433f8502
Mike Land <mikepland@gmail.com>
2014-10-13 12:08:14 -0500

    setup JSON api pull

--- SuperHeropedia/SuperHeroController.m
+++ SuperHeropedia/SuperHeroController.m
@@ -9,7 +9,8 @@
 #import "SuperHeroController.h"
 
 @interface SuperHeroController () <UITableViewDataSource, UITableViewDelegate>
-@property NSDictionary *supermanDictionary;
+@property NSArray *superHeroes;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -17,21 +18,36 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+
     // Do any additional setup after loading the view, typically from a nib.
-    self.supermanDictionary = [NSDictionary dictionaryWithObjectsAndKeys:@"Superman", @"name", @"Krypton", @"home", nil];
-    NSLog(@"%@", [self.supermanDictionary objectForKey:@"name"]);
-    NSLog(@"%@", [self.supermanDictionary objectForKey:@"home"]);
+    NSDictionary *supermanDictionary = [NSDictionary dictionaryWithObjectsAndKeys:@"Superman", @"name", @"Krypton", @"home", nil];
+    NSLog(@"%@", [supermanDictionary objectForKey:@"name"]);
+    NSLog(@"%@", [supermanDictionary objectForKey:@"home"]);
+//    self.superHeroes = [NSArray arrayWithObjects:supermanDictionary, supermanDictionary, nil];
+    NSURL *url = [NSURL URLWithString:@"https://s3.amazonaws.com/mobile-makers-lib/superheroes.json"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+        NSString *jsonString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"%@", jsonString);
+        NSError *jsonError = nil;
+        self.superHeroes = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+        [self.tableView reloadData];
+        NSLog(@"Connection Error: %@", connectionError);
+        NSLog(@"JSON Error: %@", jsonError);
+    }];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSDictionary *superHero = [self.superHeroes objectAtIndex:indexPath.row];
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MyCellID"];
-    cell.textLabel.text = [self.supermanDictionary objectForKey:@"name"];
-    cell.detailTextLabel.text = [self.supermanDictionary objectForKey:@"home"];
+    cell.textLabel.text = [superHero objectForKey:@"name"];
+    cell.detailTextLabel.text = [superHero objectForKey:@"description"];
     return cell;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 1;
+    return self.superHeroes.count;
 }
 
 @end
